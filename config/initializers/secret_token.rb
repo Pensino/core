1a38d3c8ecd9e9c3b8b4c29de5d81e1cd6d5a3dc
emilio murta <emilio.murta@msitbrasil.com>
2011-11-11 22:17:42 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CorePensino::Application.config.secret_token = '75392c7a3ffdfc06a040b60d9ae65ed1407fba821b2f951ed28919bfa9270b8fcdc0f45e4e179f1abf79b64f55e7528f43f085f68b6d9993e4dcfa4749544c90'
